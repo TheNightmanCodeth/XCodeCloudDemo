111357ea530ebd5cba8d9849c7f39aedf34996e1
Joe Diragi <TheNightmanCodeth@users.noreply.github.com>
2023-06-14 11:12:31 -0400

    Initial Commit

---
+++ XcodeCloudDemo/XcodeCloudDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudDemoApp.swift
+//  XcodeCloudDemo
+//
+//  Created by Joe Diragi on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
